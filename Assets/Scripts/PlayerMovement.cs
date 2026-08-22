452ab72b05d0396d5cdc5077e26e6991b11c4aba
Fazb3r <faiberpiedrahita@gmail.com>
2026-08-22 01:40:41 -0500

    feature:animations working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+    private static readonly int InputX = Animator.StringToHash("InputX");
+    private static readonly int InputY = Animator.StringToHash("InputY");
+    private static readonly int LastInputX = Animator.StringToHash("LastInputX");
+    private static readonly int LastInputY = Animator.StringToHash("LastInputY");
+
+
     [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
+    private Rigidbody2D _rb;
+    private Vector2 _moveInput;
+    private Animator _animator;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+        _rb.linearVelocity = _moveInput * moveSpeed;
         
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>(); 
+        _animator.SetBool(IsWalking, true);
+
+        if (context.canceled)
+        {
+            _animator.SetBool(IsWalking, false);
+            _animator.SetFloat(LastInputX, _moveInput.x);
+            _animator.SetFloat(LastInputY, _moveInput.y);
+        } 
+        _moveInput = context.ReadValue<Vector2>(); 
+        _animator.SetFloat(InputX, _moveInput.x);
+        _animator.SetFloat(InputY, _moveInput.y);
     }   
 
 }
